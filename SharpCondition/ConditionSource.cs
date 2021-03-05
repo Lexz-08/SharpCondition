72bfe12071be8f5bb9af95613d5bfc1ba578ecca
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-03-04 20:13:17 -0800

    same method, but with custom types

--- SharpCondition/ConditionSource.cs
+++ SharpCondition/ConditionSource.cs
@@ -3,7 +3,6 @@
 	public class Condition
 	{
 		public delegate void MethodToInvokeArgs(params object[] args);
-
 		public delegate void ErrorInvokeArgs(params object[] args);
 
 		public static void If(bool expression, bool requiredValue, MethodToInvokeArgs method, ErrorInvokeArgs error = null, object[] methodArgs = null, object[] errorArgs = null)
@@ -13,6 +12,21 @@
 				method.Invoke(methodArgs);
 			}
 			else if (expression != requiredValue)
+			{
+				if (error != null) error.Invoke(errorArgs);
+				else return;
+			}
+		}
+		public static void If<T>(T expression, T requiredValue, MethodToInvokeArgs method, ErrorInvokeArgs error = null, object[] methodArgs = null, object[] errorArgs = null)
+		{
+			object expressionObj = expression;
+			object requiredValueObj = requiredValue;
+
+			if (expressionObj == requiredValueObj)
+			{
+				method.Invoke(methodArgs);
+			}
+			else if(expressionObj != requiredValueObj)
 			{
 				if (error != null) error.Invoke(errorArgs);
 				else return;
